54388911916d9314f02145b19a4414b19c9e5e0d
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2020-05-09 14:51:35 +0100

    Removed the User model, its API and all references to it

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-from .user import User, AdminUser, Role
+from .user import AdminUser, Role
 from .product import Product, ProductCategory
 from .payment import PaymentLog
 from .subscriber import Subscriber
--- api/routes/user.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from flask_restful import Api
-from api.resources.user import UserAPI, UserListAPI
-
-USERS_BLUEPRINT = Blueprint('users', __name__)
-Api(USERS_BLUEPRINT).add_resource(UserListAPI, '/api/users/')
-Api(USERS_BLUEPRINT).add_resource(UserAPI, '/api/users/<int:id>/')
